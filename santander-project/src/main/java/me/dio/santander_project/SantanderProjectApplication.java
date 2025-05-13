fcab886750b8bf7161b58206419170acbd0674c2
devcelsoborges <borgesnetocs@gmail.com>
2025-05-13 16:16:50 -0300

    Add Santander Project files

---
+++ santander-project/src/main/java/me/dio/santander_project/SantanderProjectApplication.java
@@ -0,0 +1,13 @@
+package me.dio.santander_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantanderProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantanderProjectApplication.class, args);
+	}
+
+}
